229c4eca17d6ac3aa92563ec23c9074af85b21c6
FishEggsThe <120220324+FishEggsThe@users.noreply.github.com>
2025-05-18 20:29:39 -0400

    layout is created, made a user control for time inputs but no functionality yet

--- EyeBreak/MainWindow.xaml.cs
+++ EyeBreak/MainWindow.xaml.cs
@@ -11,14 +11,24 @@ using System.Windows.Shapes;
 
 namespace EyeBreak
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnAddWait_Click(object sender, RoutedEventArgs e) {
+
+        }
+        private void btnAddBreak_Click(object sender, RoutedEventArgs e) {
+
+        }
+        private void btnAddSound_Click(object sender, RoutedEventArgs e) {
+
+        }
+        private void btnStartTimer_Click(object sender, RoutedEventArgs e) {
+
         }
     }
 }
